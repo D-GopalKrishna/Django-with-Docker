0abd3433f02fe1adb19f611b0849cadbd9f80931
D Gopall Krishna <nikhil0223@gmail.com>
2021-01-25 12:53:13 +0530

    added image upload facility

--- 7-bookstore-reviews/bookstore_project/urls.py
+++ 7-bookstore-reviews/bookstore_project/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
@@ -29,4 +36,4 @@ urlpatterns = [
 
     path('books/', include('books.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)                       ## for producntion content to see media items locally 
